ec5ad56b38296d2f41ada8cd8cf8dcaa8ee52788
lyonsja <lyonsja@oregonstate.edu>
2018-02-04 18:01:41 -0800

    Updated Makefile and finalized unittest4

--- projects/lyonsja/dominion/unittest4.c
+++ projects/lyonsja/dominion/unittest4.c
@@ -47,8 +47,8 @@ int main() {
 	/*int remove1, remove2;*/
 	int seed = 1000;
 	int numPlayers = 2;
-	/*int thisPlayer = 0;
-    int otherPlayer = 1;
+	int thisPlayer = 0;
+    /*int otherPlayer = 1;
     */
 	struct gameState G;
 
@@ -59,35 +59,53 @@ int main() {
 	initializeGame(numPlayers, k, seed, &G);
 
 	printf("------------------ Testing Function: %s ------------------\n", TESTFUNC);
-       
-    
     
     /*Test case, no supply piles empty--game should continue*/
     printf("@@@@@@@@@@ Test: NO buys left @@@@@@@@@@\n\n");
+    G.coins = 5;
     G.numBuys = 0;
     
-    
-    flag = cAssert(buyCard(&G), 0, flag);
-    
-    
+    /* 11 corresponds to mine card */
+    flag = cAssert(buyCard(11, &G), -1, flag);
     
     
+    printf("@@@@@@@@@@ Test: NO cards left in supply pile @@@@@@@@@@\n\n");
+    G.numBuys = 1;
+    G.coins = 5;
     
-    printf("@@@@@@@@@@ Test: NO buys left @@@@@@@@@@\n\n");
+    printf("buyCard returned %d, expected: %d\n", buyCard(11, &G), -1);
     
-    printf("@@@@@@@@@@ Test: NO cards left @@@@@@@@@@\n\n");
+    G.numBuys = 1;
+    G.coins = 5;
+    
+    flag = cAssert(buyCard(11, &G), -1, flag);
+    
+    
     
     
     printf("@@@@@@@@@@ Test: Insufficient buying power @@@@@@@@@@\n\n");
+    G.numBuys = 1;
+    G.coins = 3;
+    
+    printf("buyCard returned %d, expected: %d\n", buyCard(11, &G), -1);
+    
+    G.numBuys = 1;
+    G.coins = 3;
+    flag = cAssert(buyCard(11, &G), -1, flag);
+    
     
     
     printf("@@@@@@@@@@ Test: valid buy from supply @@@@@@@@@@\n\n");
+    G.numBuys = 1;
+    G.coins = 5;
     
+    printf("buyCard returned %d, expected: %d\n", buyCard(11, &G), 0);
+    G.numBuys = 1;
+    G.coins = 5;
+    flag = cAssert(buyCard(11, &G), 0, flag);
     
-    
-    
-    
-    
+    printf("subTest: Card gained to discard?\n\n");
+    flag = cAssert(G.discardCount[thisPlayer], 1, flag);
     
   if(flag){
 		printf("\n %%%%%%%%%%%%%%%%%%%% SUCCESS: TESTING COMPLETE %s %%%%%%%%%%%%%%%%%%%%\n\n", TESTFUNC);
