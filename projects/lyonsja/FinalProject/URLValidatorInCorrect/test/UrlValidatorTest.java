97bcc187c2939eb1a23ecb20f90922950d9f2711
lyonsja <lyonsja@oregonstate.edu>
2018-03-19 23:42:52 -0700

    Final Commit

--- projects/lyonsja/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/lyonsja/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -7,9 +7,6 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
-
-
 public class UrlValidatorTest extends TestCase {
 
 
@@ -36,13 +33,15 @@ public class UrlValidatorTest extends TestCase {
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
-
+	 //You can use this function to implement your First Partition testing
+	   UrlValidator urlVal = new UrlValidator(null, null, 1);
+	   assertTrue(urlVal.isValid("http://aurl.org"));
    }
    
    public void testYourSecondPartition(){
 		 //You can use this function to implement your Second Partition testing	   
-
+	   UrlValidator urlVal = new UrlValidator(null, null, 1);
+	   assertTrue(urlVal.isValid("ftp://anotherurl.com"));
    }
    //You need to create more test cases for your Partitions if you need to 
    
@@ -50,7 +49,7 @@ public class UrlValidatorTest extends TestCase {
    {
 	   //You can use this function for programming based testing
 	   UrlValidator urlVal2 = new UrlValidator(null, null, 1);
-	   String str4 = "HTTP://SomeurlThatWorks.Org";
+	   String str4 = "HTTP://SomeurlThatShouldWork.Org";
 	   assertTrue(urlVal2.isValid(str4));
 	   
 	   String str5 = "https:/thisurlisbad.something";
@@ -59,22 +58,29 @@ public class UrlValidatorTest extends TestCase {
 	   String str6 = "h3t://anotherUrl.org";
 	   assertTrue(urlVal2.isValid(str6));
 	   
+	   //Testing a single capital character in the URL
 	   String str7 = "https://Acapitalletter.com";
 	   assertTrue(urlVal2.isValid(str7));
 	   
+	   //Testing single capital character in scheme section of URL"
 	   String str8 = "Https://lowercasebody.org";
 	   assertTrue(urlVal2.isValid(str8));
 	   
+	   //Testing null case
 	   assertTrue(!urlVal2.isValid("null"));
 	   
 	   assertTrue(!urlVal2.isValid("http://259.259.259"));
 	   
 	   assertTrue(urlVal2.isValid("http://166.179.200"));
 	   
-	   //Alpanumeric character combination in URL
+	   //Alphanumeric character combination in URL
 	   assertTrue(urlVal2.isValid("http://url7.com"));
 	   
+	   //IP-address type URL
 	   assertTrue(urlVal2.isValid("166.154.232"));
+	   
+	   //Scheme (in this case http) in the middle part of the URL
+	   assertTrue(urlVal2.isValid("aurl.http://someUrl"));
 	   
 	   
    }
